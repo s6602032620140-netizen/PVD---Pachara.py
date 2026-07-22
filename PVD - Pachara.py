c5dd9149123ca3c5f89834772a10135c1e906c55
s6602032620140-netizen <s6602032620140@email.kmutnb.ac.th>
2026-07-22 14:02:10 +0700

    Create PVD - Pachara.py

---
+++ PVD - Pachara.py
@@ -0,0 +1,33 @@
+PVD_APP/
+
+│ app.py
+│ requirements.txt
+│ README.md
+│
+
+├── pages
+│      Home.py
+│      PVD_Design.py
+│      Settlement.py
+│      SandMat.py
+│      Graph.py
+│
+
+├── calculations
+│      barron.py
+│      terzaghi.py
+│      carillo.py
+│      settlement.py
+│      sandmat.py
+│      spacing.py
+│      validation.py
+│
+
+├── utils
+│      unit_converter.py
+│      constants.py
+│      report.py
+│
+
+├── images
+└── data
